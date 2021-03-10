45810e2945d8f6bae176fb51e930a76bfa6ca8bd
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-10 14:27:10 -0800

    working

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
@@ -9,5 +11,5 @@ export const useLocalStorage = (key, initialValue) => {
     window.localStorage.setItem(key, JSON.stringify(value));
   };
 
-  return [storedValue];
+  return [storedValue, setStoredValue];
 };
